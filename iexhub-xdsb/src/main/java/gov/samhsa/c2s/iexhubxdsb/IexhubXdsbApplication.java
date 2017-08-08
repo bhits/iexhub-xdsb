e14e72e4084607cb3d33290c4abe7f6bf451c1b7
Shruti Rao <shruti.rao@feisystems.com>
2017-08-08 09:49:12 -0400

    Enable Discovery and Feign clients
    #14866:Create Restful services to get patient data from HIE

--- iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/IexhubXdsbApplication.java
+++ iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/IexhubXdsbApplication.java
@@ -2,8 +2,12 @@ package gov.samhsa.c2s.iexhubxdsb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class IexhubXdsbApplication {
 
 	public static void main(String[] args) {
