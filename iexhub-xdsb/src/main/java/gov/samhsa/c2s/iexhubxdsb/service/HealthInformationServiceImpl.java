cf56290bc156f125e079804fca687facc56a2894
Shruti Rao <shruti.rao@feisystems.com>
2017-08-14 14:14:24 -0400

    Remove unnecessary code
    #14866:Create Restful services to get patient data from HIE

--- iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/service/HealthInformationServiceImpl.java
+++ iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/service/HealthInformationServiceImpl.java
@@ -21,10 +21,6 @@ public class HealthInformationServiceImpl implements HealthInformationService{
     @Override
     public PatientHealthDataDto getPatientHealthDataFromHIE(String patientId) {
 
-        boolean tls = false;
-        tls = (iexhubXdsbProperties.getHieos().getXdsBRegistryEndpointURI() == null) ? false
-                : ((iexhubXdsbProperties.getHieos().getXdsBRegistryEndpointURI().toLowerCase().contains("https") ? true
-                : false));
         //Use PatientId to perform a PIX Query to get the enterprise ID
 
         //Step 2: Using the enterprise ID, perform XDS.d Registry+Repository Operation
