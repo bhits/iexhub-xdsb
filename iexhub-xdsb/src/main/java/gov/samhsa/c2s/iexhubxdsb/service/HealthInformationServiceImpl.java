4f9e43bfa6df01d8686fffbbf66b34936d0a9162
Shruti Rao <shruti.rao@feisystems.com>
2017-08-14 14:23:42 -0400

    Call xdsb registry and check for errors
    #14866:Create Restful services to get patient data from HIE

--- iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/service/HealthInformationServiceImpl.java
+++ iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/service/HealthInformationServiceImpl.java
@@ -1,8 +1,13 @@
 package gov.samhsa.c2s.iexhubxdsb.service;
 
+import gov.samhsa.acs.xdsb.registry.common.XdsbDocumentType;
+import gov.samhsa.acs.xdsb.registry.wsclient.XdsbRegistryWebServiceClient;
+import gov.samhsa.acs.xdsb.registry.wsclient.adapter.XdsbRegistryAdapter;
 import gov.samhsa.c2s.iexhubxdsb.config.IExHubXdsbProperties;
 import gov.samhsa.c2s.iexhubxdsb.service.dto.PatientHealthDataDto;
 import lombok.extern.slf4j.Slf4j;
+import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryError;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,10 +25,33 @@ public class HealthInformationServiceImpl implements HealthInformationService{
 
     @Override
     public PatientHealthDataDto getPatientHealthDataFromHIE(String patientId) {
+        String registryEndpoint = iexhubXdsbProperties.getHieos().getXdsBRegistryEndpointURI();
+        String repositoryEndpoint = iexhubXdsbProperties.getHieos().getXdsBRepositoryEndpointURI();
 
-        //Use PatientId to perform a PIX Query to get the enterprise ID
+        //Step 1: Use PatientId to perform a PIX Query to get the enterprise ID
 
-        //Step 2: Using the enterprise ID, perform XDS.d Registry+Repository Operation
+        //Step 2: Using the enterprise ID, perform XDS.b Registry Operation
+        XdsbRegistryAdapter xdsbRegistryAdapter = new XdsbRegistryAdapter(new XdsbRegistryWebServiceClient(registryEndpoint));
+        log.info("Calling XdsB Registry");
+        AdhocQueryResponse adhocQueryResponse = xdsbRegistryAdapter.registryStoredQuery(PATIENT_ID, XdsbDocumentType.CLINICAL_DOCUMENT);
+
+        //Check for errors
+        if ((adhocQueryResponse.getRegistryErrorList() != null) &&
+                (adhocQueryResponse.getRegistryErrorList().getRegistryError().size() > 0)) {
+            log.info("Calling to XdsB registry return an error");
+            log.debug("Printing error messages");
+            for (RegistryError error : adhocQueryResponse.getRegistryErrorList().getRegistryError()) {
+                log.debug("Error Code: ", error.getErrorCode());
+                log.debug("Error Code Context: ", error.getCodeContext());
+                log.debug("Error Location: ", error.getLocation());
+                log.debug("Error Severity: ", error.getSeverity());
+                log.debug("Error Value: ", error.getValue());
+            }
+            //TODO: Return an exception
+            log.info("Returning NULL");
+            return null;
+        }
+        log.info("XdsB Registry call was successful");
 
         //Step 3: Convert the obtained documents into JSON format
 
