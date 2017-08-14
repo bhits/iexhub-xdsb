7c56440a7fb17e4bfaf1c869dad27d38d73c9f20
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-08-14 18:43:51 -0400

    Create a Spring Boot project
    #14866:Create Restful services to get patient data from HIE

---
+++ iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/IexhubXdsbApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s.iexhubxdsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexhubXdsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IexhubXdsbApplication.class, args);
+	}
+}
