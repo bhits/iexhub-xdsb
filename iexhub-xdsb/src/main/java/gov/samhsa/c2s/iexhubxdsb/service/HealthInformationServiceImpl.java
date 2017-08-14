c98bed4ebbc6f569e33919457ee9bf0ac4a97e52
Shruti Rao <shruti.rao@feisystems.com>
2017-08-14 14:30:21 -0400

    Get document Ids
    #14866:Create Restful services to get patient data from HIE

--- iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/service/HealthInformationServiceImpl.java
+++ iexhub-xdsb/src/main/java/gov/samhsa/c2s/iexhubxdsb/service/HealthInformationServiceImpl.java
@@ -7,12 +7,20 @@ import gov.samhsa.c2s.iexhubxdsb.config.IExHubXdsbProperties;
 import gov.samhsa.c2s.iexhubxdsb.service.dto.PatientHealthDataDto;
 import lombok.extern.slf4j.Slf4j;
 import oasis.names.tc.ebxml_regrep.xsd.query._3.AdhocQueryResponse;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExternalIdentifierType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExtrinsicObjectType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.IdentifiableType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.LocalizedStringType;
 import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryError;
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.JAXBElement;
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 @Slf4j
-public class HealthInformationServiceImpl implements HealthInformationService{
+public class HealthInformationServiceImpl implements HealthInformationService {
 
     private final IExHubXdsbProperties iexhubXdsbProperties;
 
@@ -53,10 +61,76 @@ public class HealthInformationServiceImpl implements HealthInformationService{
         }
         log.info("XdsB Registry call was successful");
 
-        //Step 3: Convert the obtained documents into JSON format
+        //Step 3: From AdhocQuery Response, extract the document IDs
+        List<JAXBElement<? extends IdentifiableType>> documentObjects = adhocQueryResponse.getRegistryObjectList().getIdentifiable();
 
+        if ((documentObjects == null) ||
+                (documentObjects.size() == 0)) {
+            log.info("No documents found for the given Patient ID");
+            return new PatientHealthDataDto();
+        } else {
+            log.info("Some documents were found in the Registry for the given Patient ID");
+            HashMap<String, String> documents = getDocumentsFromDocumentObjects(documentObjects);
 
+            if(documents.size() ==0){
+                log.info("No XDSDocumentEntry documents found for the given Patient ID");
+                return new PatientHealthDataDto();
+            }
+            //Step 4: Using the Document IDs, perform XDS.d Repository call
+
+            //Step 5: Convert the obtained documents into JSON format
+
+        }
         return null;
-
     }
+
+    private HashMap<String, String> getDocumentsFromDocumentObjects(List<JAXBElement<? extends IdentifiableType>> documentObjects) {
+        HashMap<String, String> documents = new HashMap<>();
+
+        for (JAXBElement identifiable : documentObjects) {
+            if (identifiable.getValue() instanceof ExtrinsicObjectType) {
+                //Check if "home" attribute is present
+                String home = (((ExtrinsicObjectType) identifiable.getValue()).getHome() != null) ? ((ExtrinsicObjectType) identifiable.getValue()).getHome() : null;
+
+                List<ExternalIdentifierType> externalIdentifiers = ((ExtrinsicObjectType) identifiable.getValue()).getExternalIdentifier();
+
+                // Find the ExternalIdentifier that has the "XDSDocumentEntry.uniqueId" value...
+                String uniqueId = null;
+                for (ExternalIdentifierType externalIdentifier : externalIdentifiers) {
+
+                    boolean foundSearchValue = false;
+                    List<LocalizedStringType> localizedStringTypeList = externalIdentifier.getName().getLocalizedString();
+
+                    for (LocalizedStringType temp : localizedStringTypeList) {
+                        String searchValue = temp.getValue();
+                        if ((searchValue != null) &&
+                                (searchValue.equalsIgnoreCase("XDSDocumentEntry.uniqueId"))) {
+                            foundSearchValue = true;
+                            log.debug("Located XDSDocumentEntry.uniqueId ExternalIdentifier");
+                            uniqueId = externalIdentifier.getValue();
+                            break;
+                        }
+                    }
+                    if (foundSearchValue) {
+                        break;
+                    }
+                }
+
+                if (uniqueId != null) {
+                    documents.put(uniqueId, home);
+                    log.debug("Document ID added: " + uniqueId + ", homeCommunityId: " + home);
+                }
+            } else {
+                //TODO: Test for this case
+                log.info("Not an ExtrinsicObjectType");
+                String home = (((IdentifiableType) identifiable.getValue()).getHome() != null) ? ((IdentifiableType) identifiable.getValue()).getHome() : null;
+
+                documents.put(((IdentifiableType) identifiable.getValue()).getId(), home);
+                log.debug("Document ID added: " + ((IdentifiableType) identifiable.getValue()).getId() + ", homeCommunityId: " + home);
+            }
+        }
+        log.info("Number of XDSDocumentEntry documents found = " + documents.size());
+        return documents;
+    }
+
 }
